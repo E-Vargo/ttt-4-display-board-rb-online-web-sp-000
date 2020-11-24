37115bca3b0643a5d86576a2831e9a83005778dc
Elliott Vargo <vargoelliott@gmail.com>
2020-11-24 07:44:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = ["X","X","X"," "," ","X"," ","O"," "]
 def display_board(board)
-  row1 = " #{board[0]} | #{board[1]} | #{board[2]}  "
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
   row2 = " #{board[3]}  | #{board[4]} | #{board[5]}  "
   row3 = "  #{board[6]} |#{board[7]}  | #{board[8]}  "
   divider = "-----------"
