677ebfc06db6812b03016446a2a8700145727c72
Elliott Vargo <vargoelliott@gmail.com>
2020-11-24 07:36:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  row = "   |  |   "
+  divider = "-----------"
+puts row
+puts divider
+puts row
+puts divider
+puts row
